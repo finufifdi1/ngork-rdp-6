662f8a14873ca668dc45c933dac8917102a8a01d
finufifdi1 <110578765+finufifdi1@users.noreply.github.com>
2022-08-20 08:22:08 +0700

    Update start.bat

--- resources/start.bat
+++ resources/start.bat
@@ -1,21 +1,21 @@
 @echo off
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"Windows Server 2019 By @LohiniSaya" > out.txt 2>&1
+net config server /srvcomment:"Windows Server 2019 By yrYHTYrfybjyhty" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user @LohiniSaya yrYHTYrfybjyhty /add >nul
-net localgroup administrators @LohiniSaya /add >nul
-net user @LohiniSaya /active:yes >nul
+net user yrYHTYrfybjyhty yrYHTYrfybjyhty /add >nul
+net localgroup administrators yrYHTYrfybjyhty /add >nul
+net user yrYHTYrfybjyhty /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
-ICACLS C:\Windows\Temp /grant @LohiniSaya:F >nul
-ICACLS C:\Windows\installer /grant @LohiniSaya:F >nul
+ICACLS C:\Windows\Temp /grant yrYHTYrfybjyhty:F >nul
+ICACLS C:\Windows\installer /grant yrYHTYrfybjyhty:F >nul
 echo Successfully installed! If RDP is dead, rebuild again.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
-echo Username: @LohiniSaya
+echo Username: yrYHTYrfybjyhty
 echo Password: yrYHTYrfybjyhty
 echo You can login now
 ping -n 10 127.0.0.1 >nul
